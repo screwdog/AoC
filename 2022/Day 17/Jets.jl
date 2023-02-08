7d42922ce3575b10d82a336ca2fb9e12b9017bc3
Harry Ray <ray.harry@gmail.com>
2023-02-08 10:05:26 +0800

    2022 day 17 StatefulCycle made parametric

--- 2022/Day 17/Jets.jl
+++ 2022/Day 17/Jets.jl
@@ -1,28 +1,30 @@
 const LEFT_CHAR = '<'
 
 """
-`StatefulCycle`
+`StatefulCycle{T}`
 
 Represents an iterator that returns items from a finite collection in order
 and then repeats this process endlessly. Used to represent the sequence of
 blocks falling and the sequence of air jets.
 """
-mutable struct StatefulCycle
-    vec::Vector
+mutable struct StatefulCycle{T}
+    vec::Vector{T}
     len::Int
     curr::Int
-    StatefulCycle(v) = new(v, length(v), length(v))
+    StatefulCycle(v) = new{eltype(v)}(v, length(v), length(v))
 end
 state(sc::StatefulCycle) = sc.curr
 cyclelength(sc::StatefulCycle) = sc.len
 reset!(sc::StatefulCycle) = sc.curr = sc.len
+# Conforming fully to the Iterator interface is unnecessary but helpful during
+# development and debugging
 function Base.iterate(sc::StatefulCycle, state=nothing)
     sc.curr = mod1(sc.curr + 1, sc.len)
     return (sc.vec[sc.curr], sc.curr)
 end
 Base.IteratorSize(::StatefulCycle) = Base.IsInfinite()
 Base.IteratorEltype(::StatefulCycle) = Base.HasEltype()
-Base.eltype(sc::StatefulCycle) = eltype(sc.vec)
+Base.eltype(::StatefulCycle{T}) where T = T
 Base.isdone(sc::StatefulCycle, state=nothing) = false
 
 jets(v::Vector{Bool}) = StatefulCycle(v)
